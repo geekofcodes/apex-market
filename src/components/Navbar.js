d42c13951fcd51bc3ff8603333c69b47434e3c63
G Madhu Sudan Reddy <madhureddy2019cse@gmail.com>
2024-01-16 22:00:21 +0530

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  InputBase,
+  Menu,
+  MenuItem,
+} from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({ isLoggedIn }) => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMobileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+    setMobileMenuOpen(true);
+  };
+
+  const handleMobileMenuClose = () => {
+    setAnchorEl(null);
+    setMobileMenuOpen(false);
+  };
+
+  const menuItems = isLoggedIn
+    ? [
+        { label: 'Home', to: '/' },
+        { label: 'Account', to: '/account' },
+        { label: 'Cart', to: '/cart' },
+        { label: 'User Profile', to: '/profile' },
+      ]
+    : [{ label: 'Home', to: '/' }, { label: 'Login', to: '/login' }];
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <AppBar position="sticky">
+      <Toolbar>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          ApexMarket
+        </Typography>
 
-export default Navbar
+        <div className="hidden md:flex md:items-center md:ml-6">
+          <div className="relative">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search"
+              classes={{
+                root: 'pl-10',
+                input: 'py-2',
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+        </div>
+
+        <div className="md:hidden flex items-center">
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            onClick={handleMobileMenuOpen}
+          >
+            {mobileMenuOpen ? <CloseIcon /> : <MenuIcon />}
+          </IconButton>
+        </div>
+
+        <Menu
+          anchorEl={anchorEl}
+          open={mobileMenuOpen}
+          onClose={handleMobileMenuClose}
+        >
+          {menuItems.map((item) => (
+            <MenuItem key={item.label} onClick={handleMobileMenuClose}>
+              <Link to={item.to}>{item.label}</Link>
+            </MenuItem>
+          ))}
+        </Menu>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
--- src/pages/routes/Routes.js
+++ src/pages/routes/Routes.js
@@ -3,10 +3,14 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom/cjs/react-router-
 import Home from '../Home'
 import Login from '../authentication/Login'
 import SignUp from '../authentication/Signup'
+import Navbar from '../../components/Navbar'
 
 const Routes = () => {
+  const isLoggedIn = true;
+
   return (
     <BrowserRouter>
+      <Navbar isLoggedIn={isLoggedIn} />
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/login" exact component={Login} />
