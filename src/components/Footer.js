baa9d4aabec6a55fde2b08df63a25589b29ea77a
G Madhu Sudan Reddy <madhureddy2019cse@gmail.com>
2024-01-17 22:58:51 +0530

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-8">
       <div className="container mx-auto p-4">
-        <div className="flex flex-col md:flex-row md:justify-between">
-          <div className="mb-4 md:mb-0">
+        <div className="flex flex-col md:flex-row items-center md:justify-between">
+          <div className="mb-4 md:mb-0 sm:flex-col sm:items-center">
             <h3 className="text-2xl font-bold mb-2">Connect with us</h3>
             <div className="flex space-x-4">
               <a href="#" className="text-gray-300 hover:text-white">
