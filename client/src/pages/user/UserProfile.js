c22b6b680dfe1ccefb218b07eb63ff146d6b77e0
G Madhu Sudan Reddy <madhureddy2019cse@gmail.com>
2024-02-11 11:02:56 +0530

    added the user details in the UserProfile.js

--- client/src/pages/user/UserProfile.js
+++ client/src/pages/user/UserProfile.js
@@ -4,28 +4,36 @@ import { UserOutlined } from '@ant-design/icons';
 
 const { Title, Paragraph } = Typography;
 
-const UserProfile = ({ userId, userName, userEmail }) => {
-  const [loading, setLoading] = useState(true);
+const UserProfile = () => {
+
+  const userName = sessionStorage.getItem('userName')
+  const userEmail = sessionStorage.getItem('userEmail')
   console.log(userName)
+
   return (
-    <Row justify="center" style={{ marginTop: '20px' }}>
-      <Col xs={24} sm={20} md={16} lg={12} xl={8}>
-        <Card loading={loading}>
-          <div style={{ textAlign: 'center' }}>
-            <Avatar size={64} icon={<UserOutlined />} />
-            <Title level={3}>
-              {/* {userName ? userName : 'Loading...'} */}
-              {userName}
-            </Title>
-          </div>
-          <Paragraph style={{ textAlign: 'center' }}>
-            {/* {userEmail ? `Email: ${userEmail}` : 'Loading...'} <br /> */}
-            {/* You can add additional details here if needed */}
-            {userEmail}
-          </Paragraph>
-        </Card>
-      </Col>
-    </Row>
+    <React.Fragment>
+      <div className='m-10 h-full'>
+      <Title level={2}>User Profile</Title>
+        <Row justify="center" style={{ marginTop: '20px' }}>
+          <Col xs={24} sm={20} md={16} lg={12} xl={8}>
+            <Card>
+              <div style={{ textAlign: 'center' }}>
+                <Avatar size={64} icon={<UserOutlined />} />
+                <Title level={3}>
+                  {/* {userName ? userName : 'Loading...'} */}
+                  {userName}
+                </Title>
+              </div>
+              <Paragraph style={{ textAlign: 'center' }}>
+                {/* {userEmail ? `Email: ${userEmail}` : 'Loading...'} <br /> */}
+                {/* You can add additional details here if needed */}
+                {`Email: ${userEmail}`}
+              </Paragraph>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    </React.Fragment>
   );
 };
 
