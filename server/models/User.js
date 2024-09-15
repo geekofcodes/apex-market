d51891d8c24916abdad06e6e65fb11eb0dd2369b
G Madhu Sudan Reddy <madhureddy2019cse@gmail.com>
2024-09-15 14:06:10 +0530

    updated the user model for better funtionality

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,17 @@
 // User.js
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-});
+const userSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        isAdmin: { type: Boolean, required: true, default: false }
+    },
+    {
+        timestamps: true
+    }
+);
 
 const User = mongoose.model('User', userSchema);
 
